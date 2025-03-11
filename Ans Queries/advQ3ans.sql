bc0080fa9254db992c888443262a9e7e112ebe3e
Tejas More <tejasmore660@gmail.com>
2025-03-11 12:13:19 +0530

    adv 3rd problem query

--- Ans Queries/advQ3ans.sql
+++ Ans Queries/advQ3ans.sql
@@ -4,3 +4,20 @@ SELECT * FROM pizzas; -- pizza_id,pizza_type_id,pizza_size,price
 SELECT * FROM pizza_types; -- pizza_type_id,pizza_name,category,ingredients
 -- LEVEL : ADVANCED
 -- PROBLEM : Determine the top 3 most ordered pizza types based on revenue for each pizza category.
+
+SELECT pizza_name, revenue, category, Most_buyed_rank
+FROM
+(SELECT pizza_name, revenue, category,
+RANK()
+OVER ( PARTITION BY category ORDER BY revenue DESC ) AS Most_buyed_rank
+FROM
+(SELECT pt.pizza_name,
+ROUND(SUM(p.price * od.quantity)::INTEGER,2) AS revenue,
+pt.category
+FROM pizzas AS p
+INNER JOIN order_details AS od
+ON p.pizza_id = od.pizza_id
+INNER JOIN pizza_types AS pt
+ON pt.pizza_type_id = p.pizza_type_id
+GROUP BY pt.category ,pt.pizza_name) )
+WHERE Most_buyed_rank <= 3;
